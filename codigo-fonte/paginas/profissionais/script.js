60893aa9277efc8d3bf8ca66fbf921690f9d2273
William Rodrigues <williamrodrigues2442@gmail.com>
2023-10-29 00:51:21 -0300

    Adiciona funcionalidade para filtrar profissionais por nome e pela área de atuação

--- codigo-fonte/paginas/profissionais/script.js
+++ codigo-fonte/paginas/profissionais/script.js
@@ -1,10 +1,9 @@
 
 /**
- * Handle toast message on page.
+ * Trata o clique ao adicionar o profissional na lista de consultas.
  */
-function handleToast() {
+function handleAddProfessional() {
   const toastsTriggers = Array.from(document.querySelectorAll('#toast-btn'));
-  console.log(toastsTriggers);
   const toast = document.getElementById('toast');
 
   if (toastsTriggers.length) {
@@ -17,4 +16,27 @@ function handleToast() {
   }
 }
 
-handleToast()
+/**
+ * Trata a procura por profissionais mostrando apenas os profissionais
+ * que deem match com o texto pesquisado, pelo nome ou pela área do profissional.
+ */
+function handleSearchProfissional() {
+  const searchInput = document.getElementById('search-input');
+
+  searchInput.addEventListener('input', (event) => {
+    const allCards = Array.from(document.querySelectorAll('.col'));
+    allCards.forEach((card) => {
+      const name = card.querySelector('.card-title').textContent;
+      const area = card.querySelector('ul > li').textContent;
+      const match = new RegExp(event.target.value, 'i');
+      if (match.test(name) || match.test(area)) {
+        card.style.display = 'flex';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+}
+
+handleAddProfessional()
+handleSearchProfissional()
