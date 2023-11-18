93f8c8a0918068e55ce7b363b75bab9f10bb8770
William Rodrigues <williamrodrigues2442@gmail.com>
2023-11-17 21:42:58 -0300

    Refatoração na página de perfil

--- codigo-fonte/paginas/perfil/script.js
+++ codigo-fonte/paginas/perfil/script.js
@@ -1,3 +1,18 @@
+/**
+ * Lida com os eventos de logout na página de perfil.
+ * Desloga o usuário e redireciona para a página inicial.
+ * Apenas para usuários logados.
+ */
+function handleProfileLogout() {
+  if (Auth.isLogged()) {
+    document.getElementById('auth-profile-logout').addEventListener('click', () => {
+      Auth.logout();
+      redirectTo('../pagina-inicial/');
+    });
+  }
+}
+
 authGuard();
 changePageHeader();
 handleLogout();
+handleProfileLogout();
